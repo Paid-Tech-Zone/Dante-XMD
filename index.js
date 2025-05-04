a4e8dcb891fc8d64bc36ee760677f3eb4790fce4
TCRONEB HACKX <paidtechzone@gmail.com>
2025-05-04 17:03:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
  Licensed under the  GPL-3.0 License;
  You may not sell this script.
  It is supplied in the hope that it may be useful.
- * @project_name : Free Bot script
- * @author : Malvin King <https://github.com/kingmalvn>
+ * @project_name : DANTE XMD 
  * @description : A Multi-functional whatsapp bot script.
  * @version 3.0.0
  **/
