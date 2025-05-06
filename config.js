0a87374d0a7867b2d38ffccbc3ba090b99c1368a
TCRONEB HACKX <paidtechzone@gmail.com>
2025-05-07 01:35:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID,
     ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1724661875852_gwwMRtTtz.png",
-    ALIVE_MSG: process.env.ALIVE_MSG || "> *OCTA ALIVE*",
+    ALIVE_MSG: process.env.ALIVE_MSG || "> *DANTE ALIVE*",
     OWNER_NAME: process.env.OWNER_NAME || "Eypz",
-    BOT_NAME: process.env.BOT_NAME || "OCTA",
+    BOT_NAME: process.env.BOT_NAME || "DANTE-XMD",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     MODE: process.env.MODE || "public" // Add this line for public/private mode
 };
